8961af25bf972989a7bd45df1c2a3cf67cc627a1
rachwongrw <rwrachelwong@gmail.com>
2021-01-28 11:57:31 -0800

    code refactor

--- app/src/main/java/com/example/monstermatch/MainActivity.kt
+++ app/src/main/java/com/example/monstermatch/MainActivity.kt
@@ -7,22 +7,10 @@ import com.example.monstermatch.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val heads: List<Int> = listOf(
-        R.drawable.monster1_head,
-        R.drawable.monster2_head,
-        R.drawable.monster3_head
-    )
-
-    private val bodies: List<Int> = listOf(
-        R.drawable.monster1_body,
-        R.drawable.monster2_body,
-        R.drawable.monster3_body
-    )
-
-    private val feet: List<Int> = listOf(
-        R.drawable.monster1_feet,
-        R.drawable.monster2_feet,
-        R.drawable.monster3_feet
+    private val monsters: List<Monster> = listOf(
+        Monster(R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
+        Monster(R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
+        Monster(R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +24,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun shuffleMonsters() {
-        binding.monsterHeadView.setImageResource(heads.random())
-        binding.monsterBodyView.setImageResource(bodies.random())
-        binding.monsterFeetView.setImageResource(feet.random())
+        binding.monsterHeadView.setImageResource(monsters.random().head)
+        binding.monsterBodyView.setImageResource(monsters.random().body)
+        binding.monsterFeetView.setImageResource(monsters.random().feet)
     }
+
+    data class Monster (val head: Int, val body: Int, val feet: Int)
 }
