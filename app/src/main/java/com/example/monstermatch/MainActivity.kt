622ae1df06911ff917028a65e665d3f9826a7321
rachwongrw <rwrachelwong@gmail.com>
2021-01-28 14:37:59 -0800

    add matching functionality

--- app/src/main/java/com/example/monstermatch/MainActivity.kt
+++ app/src/main/java/com/example/monstermatch/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.monstermatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.monstermatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val monsters: List<Monster> = listOf(
-        Monster(R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
-        Monster(R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
-        Monster(R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
+        Monster(1,"Barry", R.drawable.monster1_head, R.drawable.monster1_body, R.drawable.monster1_feet),
+        Monster(2,"Garry", R.drawable.monster2_head, R.drawable.monster2_body, R.drawable.monster2_feet),
+        Monster(3, "Larry", R.drawable.monster3_head, R.drawable.monster3_body, R.drawable.monster3_feet)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +25,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun shuffleMonsters() {
-        binding.monsterHeadView.setImageResource(monsters.random().head)
-        binding.monsterBodyView.setImageResource(monsters.random().body)
-        binding.monsterFeetView.setImageResource(monsters.random().feet)
+        var head = monsters.random()
+        var body = monsters.random()
+        var feet = monsters.random()
+
+        binding.monsterHeadView.setImageResource(head.head)
+        binding.monsterBodyView.setImageResource(body.body)
+        binding.monsterFeetView.setImageResource(feet.feet)
+
+        var headId = head.id
+        var bodyId = body.id
+        var feetId = feet.id
+
+        if (headId === bodyId && bodyId === feetId) {
+            binding.msgText.text = "You match " + head.name
+        } else {
+            binding.msgText.text = ""
+        }
+
+
     }
 
-    data class Monster (val head: Int, val body: Int, val feet: Int)
+    data class Monster (val id: Int, val name: String, val head: Int, val body: Int, val feet: Int)
 }
